96165b249dd18f55351215e454912e17f4d10e21
ahad30 <mohimin.bd.ahad@gmail.com>
2025-09-02 08:23:59 +0600

    asdd

--- client/src/Routes/ProtectedRoute.jsx
+++ client/src/Routes/ProtectedRoute.jsx
@@ -4,7 +4,8 @@ import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
-  return user ? children : <Navigate to="/login" />;
+  const token = localStorage.getItem('token');
+  return user && token ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
